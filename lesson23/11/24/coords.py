e4e3275890ccddf687d5167cebe5e949a232ab6e
lolkile <arskirgame@gmail.com>
2024-11-23 15:39:24 +0300

    изменения
    изменения какие?

---
+++ lesson23/11/24/coords.py
@@ -0,0 +1,12 @@
+x = int(input('введите x:'))
+y = int(input('введите y:'))
+
+
+if x > 0 and y > 0:
+    print('i')
+elif x < 0 and y > 0:
+    print('ii')
+elif x < 0 and y < 0:
+    print('iii')
+elif x < 0 and y < 0:
+    print('iv')
